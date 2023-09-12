0eb5db358ef71c7f24e7688e2930755aad7dc3f0
Pedroiga <100247785+Pedroiga@users.noreply.github.com>
2023-09-11 21:15:05 -0300

    11/09/2023

--- js/script.js
+++ js/script.js
@@ -4,9 +4,16 @@ function typeWriter(elemento) {
     textoArray.forEach((letra, i) => {
         setTimeout(() => {
             elemento.innerHTML += letra;
-        }, 75 * i);
+        }, 100 * i);
     });
-}
+    }
 
-const titulo = document.querySelector('.digitando');
-typeWriter(titulo);
+    const titulo = document.querySelector('.digitando');
+    typeWriter(titulo);
+
+const ativaMenu = document.querySelector('.fa-bars')
+const navMenu = document.querySelector('header .menu')
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
+});
