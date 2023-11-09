1e4b55c55d1e67bc7075d73bfd75841d0839cffc
Pedro Henrique <100247785+PHenrique-dev@users.noreply.github.com>
2023-11-08 21:03:23 -0300

    Final

---
+++ script.js
@@ -0,0 +1,32 @@
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => {
+            elemento.innerHTML += letra;
+        }, 100 * i);
+    });
+    }
+
+    const titulo = document.querySelector('.digitando');
+    typeWriter(titulo);
+
+const ativaMenu = document.querySelector('.fa-bars')
+const navMenu = document.querySelector('header .menu')
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
+});
+function abrirAlertaSuccess(){
+    swal({
+        title: "Mensagem enviada com sucesso",
+        icon: "success",
+        })
+}
+function abrirAlertaError(){
+    swal({
+        title: "Algo deu errado, verifique",
+        icon: "error",
+        })
+}
+
